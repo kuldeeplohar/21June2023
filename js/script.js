ed58617d81e0fb202fbf25a2dd85738c6f7f61d9
kuldeeplohar <loharkuldeep169@gmail.com>
2023-07-12 11:46:58 +0530

    l

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+//spread oprator
+
+ var x = ["kuldeep"]
+
+ var y = [...x,'lohar']
+
+ console.log(y)
+
+ var news1 = [ 'the']
+ var n2 = [...news1,'extension']
+ var n3 = [...n2,'of Mishra']
+ var n4 = [...n3,'was']
+ var n5 = [...n4,'challenged']
+ var n6 = [...n5,' by']
+ var n7 = [...n6,'bundle']
+ var n8 = [...n7,'of']
+ var n9 = [...n8,'petitions,']
+ var n10 = [...n9,'which']
+ var n11 = [...n10,'banked']
+ var n12 = [...n11,'on']
+ var n13 = [...n12,'the']
+ var n14 = [...n13,'September']
+ var n15 = [...n14,'2021']
+ var n16 = [...n15,'order']
+ var n17 = [...n16,'the']
+ var n18 = [...n17,'top']
+ var n19 = [...n18,' court']
+ var n20 = [...n19,'The ']
+ var n21 = [...n20,'petitioners']
+ var n22 = [...n21,'included']
+ var n23 = [...n22,'Congress']
+ var n24 = [...n23,' leaders']
+ var n25 = [...n24,' Randeep']
+ var n26 = [...n25,'Singh ']
+ var n27 = [...n26,'Surjewala,']
+ var n28 = [...n27,'Jaya']
+ var n29 = [...n28,'Thakur,']
+ var n30 = [...n29,' and']
+ var n31 = [...n30,'Trinamool']
+ var n32 = [...n31,'Congress']
+ var n33 = [...n32,'MP']
+ var n34 = [...n33,'Mahua']
+ var n35 = [...n34,'Moitra']
+ var n36 = [...n35,'among']
+ var n37 = [...n36,' others.']
+
+ console.log(n37)
---
+++ js/script2.js
@@ -0,0 +1,10 @@
+
+//Rest oprator
+
+function getMyDetails(...k){
+     
+    console.log(...k);
+
+}
+
+getMyDetails('kuldeep',`lohar`,24,'neemuch')
